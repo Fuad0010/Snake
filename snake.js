04141bf51831c61b07cf1f8676fc22c7bd9dce45
Fuad0010 <fuadft@code.edu.az>
2022-08-19 16:13:51 +0400

    feat:move snake

--- snake.js
+++ snake.js
@@ -1,24 +1,49 @@
-export const SNAKE_SPEED = 1;
+import { getInputDirection } from "./assets/input.js";
+
+export const SNAKE_SPEED = 2;
 const snakeBody = [
-    { x: 10, y: 11 },
-    { x: 11, y: 11 },
-    { x: 12, y: 11 }
-]
+    { x: 11, y: 11 }
+]   
+const snakeHead = [
+    { x: 11, y: 11 }
+]   
 
 export function update(){
+    const inputDirection = getInputDirection();
     for (let i = snakeBody.length - 2; i >= 0; i--){
         snakeBody[i + 1] = { ...snakeBody[i]}
     }
-    console.log('update snake')
+    snakeBody[0].x += inputDirection.x;
+    snakeBody[0].y += inputDirection.y;
+
+    for (let i = snakeHead.length - 2; i >= 0; i--){
+        snakeHead[i + 1] = { ...snakeHead[i]}
+    }
+    snakeHead[0].x += inputDirection.x;
+    snakeHead[0].y += inputDirection.y;
 }
 
 export function draw(gameBoard){
     snakeBody.forEach(segment => {
+
+        const snakeHeadImg = document.createElement('img');
+          snakeHeadImg.src = "/assets/img/snakeHead.png";
+        
         const snakeElement = document.createElement('img');
         snakeElement.src = "/assets/img/snakeBody.png";
+
         snakeElement.style.gridRowStart = segment.x;
         snakeElement.style.gridColumnStart = segment.y;
+        
+        snakeHeadImg.style.gridRowStart = segment.x;
+        snakeHeadImg.style.gridColumnStart = segment.y;
+        
         snakeElement.classList.add('snake');
+        snakeHeadImg.classList.add('snakeHeadImg');
+        
         gameBoard.appendChild(snakeElement);
+        gameBoard.appendChild(snakeHeadImg);
+
+        
     })
 }
